ab6db03f2b6af2af22cbe4cd58e42033d8515c42
Trevor <trevor.elvey@gmail.com>
2016-01-22 16:21:52 -0800

    write 5 and 15 code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 var pingPong = function(listUp) {
   var array = []
   for (var i = 1; i <= listUp; i ++) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 ===0) {
+      array.push("pingpong");
+    } else if (i % 5 === 0) {
+      array.push("pong");
+    } else if (i % 3 === 0) {
       array.push("ping");
     } else {
       array.push(i);
--- spec/specs.js
+++ spec/specs.js
@@ -3,11 +3,11 @@ describe('pingPong', function() {
       expect(pingPong(3)).to.eql([1, 2, "ping"])
     });
 
-  //  it("is a multiple of 5", function () {
-  //    expect(pingPong(5)).to.equal([1, 2, ping, 4, pong])
-  //  });
-   //
-  //  it("is a multiple of 15", function () {
-  //    expect(pingPong(15)).to.equal([1, 2, ping, 4, pong, ping, 7, 8, ping, pong, 11, ping, 13, 14, pingpong])
-  //  });
+   it("is a multiple of 5, makes it pong", function () {
+     expect(pingPong(5)).to.eql([1, 2, "ping", 4, "pong"])
+   });
+
+   it("is a multiple of 15, makes it pingpong", function () {
+     expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"])
+   });
  });
